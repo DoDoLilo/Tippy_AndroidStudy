8e88cffea98f223a2a8956c9ba42c4189fcd9bdc
DoDoLilo <1479953662@qq.com>
2022-02-22 20:25:03 +0800

    implement the basic functions of a Tip Calculator

--- app/src/main/java/com/dodolilo/tippy/MainActivity.kt
+++ app/src/main/java/com/dodolilo/tippy/MainActivity.kt
@@ -2,6 +2,8 @@ package com.dodolilo.tippy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.i(TAG, "onPregressChanged $progress")
                 tvTipPercent.text = "$progress%"
+                computeTipAndTotal()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -39,5 +42,35 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
 
         })
+
+        etBaseAmount.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i(TAG, "afterDollarChanged $s")
+                computeTipAndTotal()
+            }
+
+        })
+    }
+
+    private fun computeTipAndTotal() {
+        if(etBaseAmount.text.isEmpty()){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        //1.get the value of percent and base amount
+        val baseAmount = etBaseAmount.text.toString().toDouble()
+        val tipPercent = seekBarTip.progress
+        //2.calculate the total amount and tip amount
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount =  baseAmount + tipAmount
+        //3.update the UI to user
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text = "%.2f".format(totalAmount)
+
     }
 }
